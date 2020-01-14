3530a438d8e2b7c60b76e9281db25f3f38cd7c97
Taylor Swinton <taylorswinton1@gmail.com>
2020-01-14 03:30:17 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,25 +1,30 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-const review = ({headline, byline, link, summary_short}) => {
-    return (
-        <div key={headline} className="review">
-                <h1><a className="review-link" href={link.url}>{headline}</a></h1>
-                <h3 className="author">{byline}</h3>
-            <br />
-
-            <p>{summary_short}</p>
-        </div>
-    )
+const Review = ({headline, byline, link, summary_short}) => {
+  return (
+    <div key={headline} className="review">
+      <header>
+        <a className="review-link" href={link.url}>
+          {headline}
+        </a>
+        <br />
+        <span className="author">{byline}</span>
+      </header>
+      <blockquote>{summary_short}</blockquote>
+    </div>
+  )
 }
 
-MovieReviews.defaultProps = {
-    reviews: []
-}
+
 
 const MovieReviews = ({reviews}) => (
-    <div className="review-list">
-        {reviews.map(review)}
-    </div>
+  <div className="review-list">
+    {reviews.map(Review)}
+  </div>
 )
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
 
 export default MovieReviews
